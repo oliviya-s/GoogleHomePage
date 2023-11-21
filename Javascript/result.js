fe48b15ecc00b97fe9fb36c483524abc0b00eb91
OliviyaS <oliviya.welkin@gmail.com>
2023-11-21 11:51:51 +0530

    added folders and files

---
+++ Javascript/result.js
@@ -0,0 +1,33 @@
+function showResult() {
+    var searchIp = $("#searchIp").val();
+    $.getJSON("http://127.0.0.1:5500/JsonFile/content.json", function(data) {
+     var result = "";
+     $.each(data, function(key, value) {
+        if (value.keyword && value.keyword.some(keyword => keyword.toLowerCase() === searchIp.toLowerCase())) 
+           {
+             result += "<div class='result_item'>";
+             result += "<img class= 'result_img' src='"  + value.image + "' alt='" + value.name + "' >";
+             result += "<div class='result_text'>" +
+                    "<button class='result_button' data-link='" + value.link + "'>" + value.name + "</button>" +
+                    "</div>";
+             result += "<div class= 'result_description' >" + value.description + "<div>";
+             result += "</div>";
+
+         }
+     });
+     if (result == "") {
+         result = "No result found!";
+     }
+    $("#resultModalBody").html(result);
+    $("#resultModal").modal("show");
+    
+
+    $(".result_button").off("click").on("click", function() {
+        var link = $(this).data("link");
+        window.open(link, "_blank");
+    });
+
+    
+ });
+}
+
